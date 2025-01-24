7de7a444a030482e05aa8f1118d28278fde654e3
wawawa1023 <miyaryousuke1023@gmail.com>
2025-01-24 19:33:00 +0900

    一覧表示追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   # Defines the root path route ("/")
   # root "articles#index"
+  root "users#index" #rootとはルートパス（今回だとhttp://localhost:3000/）を設定
+  resources :users
 end
