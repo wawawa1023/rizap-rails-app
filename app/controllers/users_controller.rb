7700030f62e64d109d9d07552ae18f1aae3f2aba
wawawa1023 <miyaryousuke1023@gmail.com>
2025-02-01 16:49:12 +0900

    CRUD操作の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,10 @@
 class UsersController < ApplicationController
-
+    before_action :set_user, only: [:show, :edit, :update, :destroy]
     def index
         @users = User.all
     end
 
     def show
-        @user = User.find(params[:id])
     end
 
     def new
@@ -15,13 +14,32 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            redirect_to users_path
+            redirect_to users_path, notice: "タスクを作成しました!"
         else
             render 'new'
         end
     end
 
+    def edit
+    end
+
+    def update
+        if @user.update(user_params)
+            redirect_to users_path, notice: "タスクを更新しました!"
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @user.destroy
+        redirect_to users_path, notice: "タスクを削除しました!"
+    end
     private
+        def set_user
+            @user = User.find(params[:id])
+        end
+        
         def user_params
             params.require(:user).permit(:name, :ruby)
         end
