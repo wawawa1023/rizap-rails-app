b087947d68237fe9afcd98ca006d2fff6093d113
wawawa1023 <miyaryousuke1023@gmail.com>
2025-02-01 21:16:34 +0900

    titleカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_01_24_075602) do
+ActiveRecord::Schema[7.0].define(version: 2025_02_01_081201) do
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.string "ruby"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
   end
 
 end
