2e6d732f024db77f7c28cb5896e9b1aa66f127c8
wawawa1023 <miyaryousuke1023@gmail.com>
2025-02-01 22:27:50 +0900

    状態を定義するカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_02_01_081201) do
+ActiveRecord::Schema[7.0].define(version: 2025_02_01_130527) do
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.string "ruby"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title"
+    t.text "description"
+    t.boolean "completed"
   end
 
 end
